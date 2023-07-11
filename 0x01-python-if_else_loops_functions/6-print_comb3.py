4be986648b6cf5a4ed465ce781d382e3a9c1a92f
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-11 10:10:30 -0700

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j:02d}", end=", " if i != 8 or j != 9 else "\n")
+        print("{0}{1:02d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
