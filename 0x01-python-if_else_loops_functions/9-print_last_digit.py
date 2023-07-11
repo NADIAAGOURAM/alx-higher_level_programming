d8392e2872f5c8b1015b352598c279bd72078f82
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-11 11:12:00 -0700

    commit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-         number = abs(number)
-    digit = number % 10
+    digit = abs(number) % 10
     print("{:d}".format(digit), end='')
     return (digit)
