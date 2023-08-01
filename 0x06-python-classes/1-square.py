8a25340aaab8c301d8b8f11743d85e9db6fc67d1
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-08-01 12:14:37 -0700

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines class square """
+
+
+class Square:
+    """coding a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
