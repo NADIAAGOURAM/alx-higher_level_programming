d3601afc5b65efc89c6ae6d87014550f768a2513
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-08-13 05:54:07 -0700

    commit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-function that prints a square with the character #
+Module print_square
+Prints a square with the character #.
 """
 
 
 def print_square(size):
-     """Prints a square where size is
+    """Prints a square where size is
     the length and width of the square.
     """
 
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        print("#" * size)
+        for j in range(size):
+            print('#', end='')
+        print()
